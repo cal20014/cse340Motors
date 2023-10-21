cefe69f4aff2a2e9689c996b8d77ae2cb6d7f570
cal20014 <cal20014@byui.edu>
2023-10-21 12:51:39 -0600

    Fixed Mobile menu & Nav

--- utilities/index.js
+++ utilities/index.js
@@ -2,81 +2,54 @@ const invModel = require("../models/inventory-model");
 const Util = {};
 
 /* ************************
- * Constructs the nav HTML unordered list
+ * Constructs the nav HTML unordered list with hamburger menu
  ************************** */
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications();
-  let list = '<ul class="nav-list">';
+  let list = '<div class="nav">';
+
+  // Replace the existing hamburger menu with the new button
+  list += '<button id="menu-toggle" class="menu">';
+  list +=
+    '<img class="ham-menu icon active" src="/images/site/hamburger.svg" alt="Hamburger Menu icon" width="40" height="40" />';
+  list +=
+    '<img class="x-menu icon" src="/images/site/close.svg" alt="X symbol to close the hamburger menu" width="40" height="40" />';
+  list += "</button>";
+
+  list += '<ul class="nav-list">';
   list +=
     '<li class="list-item"><a href="/" title="Home page" class="link">Home</a></li>';
+
   data.rows.forEach((row) => {
     list += '<li class="list-item">';
-    list +=
-      '<a class="link" href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>";
+    list += `<a class="link" href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a>`;
     list += "</li>";
   });
+
   list += "</ul>";
+  list += "</div>";
+
+  // Update the event listener for the new button
+  list += `<script>
+    document.addEventListener("DOMContentLoaded", function() {
+      const menuToggle = document.querySelector("#menu-toggle");
+      const hamMenuIcon = document.querySelector(".ham-menu");
+      const xMenuIcon = document.querySelector(".x-menu");
+      const navList = document.querySelector(".nav-list");
+
+      menuToggle.addEventListener("click", function() {
+        // Toggle the icons
+        hamMenuIcon.classList.toggle("active");
+        xMenuIcon.classList.toggle("active");
+        
+        // Toggle the nav list
+        navList.classList.toggle("active");
+      });
+    });
+  </script>`;
+
   return list;
 };
-
-// /* **************************************
-//  * Build the classification view HTML
-//  * ************************************ */
-// Util.buildClassificationGrid = async function (data) {
-//   let grid;
-//   if (data.length > 0) {
-//     grid = '<ul id="inv-display">';
-//     data.forEach((vehicle) => {
-//       grid += "<li>";
-//       grid +=
-//         '<a href="../../inv/detail/' +
-//         vehicle.inv_id +
-//         '" title="View ' +
-//         vehicle.inv_make +
-//         " " +
-//         vehicle.inv_model +
-//         'details"><img src="' +
-//         vehicle.inv_thumbnail +
-//         '" alt="Image of ' +
-//         vehicle.inv_make +
-//         " " +
-//         vehicle.inv_model +
-//         ' on CSE Motors" /></a>';
-//       grid += '<div class="namePrice">';
-//       grid += "<hr />";
-//       grid += "<h2>";
-//       grid +=
-//         '<a href="../../inv/detail/' +
-//         vehicle.inv_id +
-//         '" title="View ' +
-//         vehicle.inv_make +
-//         " " +
-//         vehicle.inv_model +
-//         ' details">' +
-//         vehicle.inv_make +
-//         " " +
-//         vehicle.inv_model +
-//         "</a>";
-//       grid += "</h2>";
-//       grid +=
-//         "<span>$" +
-//         new Intl.NumberFormat("en-US").format(vehicle.inv_price) +
-//         "</span>";
-//       grid += "</div>";
-//       grid += "</li>";
-//     });
-//     grid += "</ul>";
-//   } else {
-//     grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>';
-//   }
-//   return grid;
-// };
 
 Util.buildClassificationGrid = async function (data) {
   let grid = "";
