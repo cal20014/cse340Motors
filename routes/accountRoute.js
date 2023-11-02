bb66b2ba884e3b1f14edbdd1e546de776380c021
cal20014 <cal20014@byui.edu>
2023-11-01 22:46:22 -0600

    Added the registration route

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -13,4 +13,12 @@ const utilities = require("../utilities");
  *************************/
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+/* ***********************
+ * Deliver Registration View
+ *************************/
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
+);
+
 module.exports = router;
