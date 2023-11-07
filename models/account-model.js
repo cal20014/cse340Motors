13f82faab1eee26ab653be9d69d5a99e5c30a08b
cal20014 <cal20014@byui.edu>
2023-11-07 14:23:17 -0700

    Add email validation and registration rules

--- models/account-model.js
+++ models/account-model.js
@@ -11,7 +11,7 @@ async function registerAccount(
 ) {
   try {
     const sql =
-      "INSERT INTO account (account_firstname, account_lastname, account_email, account_password) VALUES ($1, $2, $3, $4, 'Client') RETURNING *";
+      "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *";
     return await pool.query(sql, [
       account_firstname,
       account_lastname,
@@ -23,4 +23,17 @@ async function registerAccount(
   }
 }
 
-module.exports = { registerAccount };
+/* **********************
+ *   Check for existing email
+ * ********************* */
+async function checkExistingEmail(account_email) {
+  try {
+    const sql = "SELECT * FROM account WHERE account_email = $1";
+    const email = await pool.query(sql, [account_email]);
+    return email.rowCount;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+module.exports = { registerAccount, checkExistingEmail };
