c591696b8aa38ec31c0644589cc90c13a82a24e9
cal20014 <cal20014@byui.edu>
2023-11-10 17:09:39 -0700

    Add validation rules for classification and inventory item forms

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -24,7 +24,7 @@ validate.registationRules = () => {
     body("account_email")
       .trim()
       .isEmail()
-      .normalizeEmail() // refer to validator.js docs
+      .normalizeEmail()
       .withMessage("A valid email is required.")
       .custom(async (account_email) => {
         const emailExists = await accountModel.checkExistingEmail(
@@ -65,6 +65,149 @@ validate.checkRegData = async (req, res, next) => {
       account_firstname,
       account_lastname,
       account_email,
+    });
+    return;
+  }
+  next();
+};
+
+/* ******************************
+ * Add Classification Validation Form Rules
+ * ***************************** */
+validate.addClassificationRules = () => {
+  return [
+    body("classification_name")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Classification name is required.")
+      .matches(/^[A-Za-z0-9]+$/, "i")
+      .withMessage(
+        "Classification name must not contain spaces or special characters."
+      ),
+  ];
+};
+
+/* ******************************
+ * Add Inventory Item Validation Form Rules
+ * ***************************** */
+
+validate.addInventoryItemRules = () => {
+  return [
+    body("inv_make")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Make is required")
+      .matches(/[A-Za-z0-9 ]+/i) // regex to allow letters, numbers, and spaces
+      .withMessage("Make can only contain letters, numbers and spaces."),
+    body("inv_model")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Model is required")
+      .matches(/[A-Za-z0-9 ]+/i) // regex to allow letters, numbers, and spaces
+      .withMessage("Model can only contain letters, numbers and spaces."),
+    body("inv_year")
+      .trim()
+      .isLength({ min: 4, max: 4 })
+      .withMessage("Year is required")
+      .isNumeric()
+      .withMessage("Year can only contain numbers."),
+    body("inv_description")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Description is required"),
+    body("inv_image")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Image is required")
+      .matches(/\.(png|jpg|jpeg|webp)$/)
+      .withMessage("Image must be a png, jpg, jpeg, or webp file format."),
+    body("inv_thumbnail")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Thumbnail is required")
+      .matches(/\.(png|jpg|jpeg|webp)$/)
+      .withMessage("Thumbnail must be a png, jpg, jpeg, or webp file format."),
+    body("inv_price")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Price is required")
+      .isFloat({ min: 0.0 })
+      .withMessage("Price must be a positive number."),
+    body("inv_miles")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Miles is required")
+      .isNumeric()
+      .withMessage("Miles must be a number."),
+    body("inv_color")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Color is required")
+      .matches(/[A-Za-z ]+/)
+      .withMessage("Color can only contain letters and spaces."),
+    body("classification_id")
+      .trim()
+      .isNumeric()
+      .withMessage("Classification ID is required and must be numeric."),
+  ];
+};
+
+/* ******************************
+ * Check data and return errors or continue to add classification
+ * ***************************** */
+
+validate.checkAddClassificationData = async (req, res, next) => {
+  const { classification_name } = req.body;
+  let errors = [];
+  errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav();
+    res.render("inventory/add-classification", {
+      errors,
+      title: "Add Classification",
+      nav,
+      classification_name,
+    });
+    return;
+  }
+  next();
+};
+
+/* ******************************
+ * Check data and return errors or continue to add inventory item
+ * ***************************** */
+
+validate.checkAddInventoryItemData = async (req, res, next) => {
+  const {
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id,
+  } = req.body;
+  let errors = [];
+  errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav();
+    res.render("inventory/add-inventory-item", {
+      errors,
+      title: "Add Inventory Item",
+      nav,
+      inv_make,
+      inv_model,
+      inv_year,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_miles,
+      inv_color,
+      classification_id,
     });
     return;
   }
